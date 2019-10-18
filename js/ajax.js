9e51903fabf069a0fcdb147b37a3bd1cb6265a94
Luiz Felipe <lfcbantunes@sga.pucminas.br>
2019-10-18 18:08:50 -0300

    Nova versão

--- js/ajax.js
+++ js/ajax.js
@@ -1,14 +1,14 @@
 let content = document.getElementById('ajax-content')
+
 function fetchContent(el) {
     let view = el.getAttribute('a-view')
     let folder = el.getAttribute('a-folder')
-    fetch('/ajax/${folder}/${view}.html')
-    .then(response => {
-        let html = response.text()
-        return html
-    })
-    .then(html => { 
-        console.log(html)
-        content.innerHTML = '<p> OLA MUNDO </p>'
-    })
+    fetch(`../ajax/${folder}/${view}.html`)
+        .then(response => {
+            let html = response.text()
+            return html
+        })
+        .then(html => { 
+            content.innerHTML = html
+        })
 }
